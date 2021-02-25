2deaa96012bce7e2d833cfc5d4d316cc2fda5bd2
Y <>
2021-02-25 17:51:48 +0900

    applicationcontroller修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+  class AuthenticationError < StandardError; end
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_422
+  rescue_from AuthenticationError, with: :not_authenticated
+
+  def authenticate
+    raise AuthenticationError unless current_user
+  end
+
+  def current_user
+    @current_user ||= Jwt::UserAuthenticator.(request.headers)
+  end
+
+  private
+
+  def render_422(exception)
+    render json: { error: { messages: exception.record.errors.full_messages } }, status: :unprocessable_entity
+  end
+
+  def not_authenticated
+    render json: { error: { messages: ['ログインしてください'] } }, status: :unauthorized
+  end
 end
