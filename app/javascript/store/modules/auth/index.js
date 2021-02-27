0ee771c637093f6e43d5bebe86502a7b2c9a4120
Y <>
2021-02-27 21:41:50 +0900

    store 追加

---
+++ app/javascript/store/modules/auth/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+const state = {
+    currentUser: null,
+};
+
+const getters = {
+    currentUser: state => state.currentUser,
+};
+
+const mutations = {
+    SET_CURRENT_USER: (state, user) => {
+        state.currentUser = user;
+        localStorage.setItem('currentUser', JSON.stringify(user))
+        axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`
+    },
+    CLEAR_CURRENT_USER: () => {
+        state.currentUser = null
+        localStorage.removeItem('currentUser')
+        location.reload()
+    }
+};
+
+const actions = {
+    async login({ commit }, sessionParams) {
+        const res = await axios.post(`/api/session`, sessionParams)
+        commit("SET_CURRENT_USER", res.data.user);
+    },
+
+    logout({ commit }) {
+        commit("CLEAR_CURRENT_USER");
+    },
+};
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+}; 
