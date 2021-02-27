a0b1f2c35129cdea0caa8c9bb9770ba62193d1bd
Y <>
2021-02-27 22:35:12 +0900

    create micropost table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_064559) do
+ActiveRecord::Schema.define(version: 2021_02_27_132255) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -21,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_02_25_064559) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
