fb089ddbe0cf4b209854a4321721ea6959639572
Y <>
2021-02-27 22:50:26 +0900

    create test成功

--- spec/requests/api/microposts_request_spec.rb
+++ spec/requests/api/microposts_request_spec.rb
@@ -16,4 +16,21 @@ RSpec.describe "Api::Microposts", type: :request do
       })
     end
   end
+
+  describe 'POST /api/microposts' do
+    let!(:user) { create(:user) }
+    let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
+    let(:headers) { { Authorization: "Bearer #{token}"} }
+    let(:micropost_params) { { micropost: { content: 'hoge' } } }
+    it 'ユーザーが作成できること' do
+      post api_microposts_path, params: micropost_params, headers: headers
+      expect(response).to have_http_status(201)
+      json = JSON.parse(response.body)
+      expect(json['micropost']).to include({
+                                               'id' => be_present,
+                                               'content' => 'hoge',
+                                               'user' => include('id' => user.id)
+                                           })
+    end
+  end
 end
