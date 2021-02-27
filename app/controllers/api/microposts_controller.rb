bd72fdf1be1032f94a10bfacd6ecb09639ffcc31
Y <>
2021-02-27 22:50:11 +0900

    add create action

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,4 +1,5 @@
 class Api::MicropostsController < ApplicationController
+  before_action :authenticate, only: %i[create]
 
   def index
     microposts = Micropost.includes(:user)
@@ -6,11 +7,18 @@ class Api::MicropostsController < ApplicationController
   end
 
   def create
+    micropost = current_user.microposts.create!(micropost_params)
+    render json: micropost, serializer: MicropostSerializer, status: :created
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
