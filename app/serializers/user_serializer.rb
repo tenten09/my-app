ff7e7fefc39826643c38e706bdc6e8c0ed576d91
Y <>
2021-02-28 16:19:35 +0900

    placeholder 追加

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,8 +2,10 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :introduction, :avatar_url
 
   def avatar_url
-    return nil unless object.avatar.attached?
-
-    Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
+    if object.avatar.attached?
+      Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true)
+    else
+      'https://placehold.jp/300x300.png'
+    end
   end
 end
