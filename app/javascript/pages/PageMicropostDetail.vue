8ff2605ef0201c9014832760a0f4910288151bb3
Y <>
2021-02-28 14:06:53 +0900

    isMine

--- app/javascript/pages/PageMicropostDetail.vue
+++ app/javascript/pages/PageMicropostDetail.vue
@@ -1,30 +1,38 @@
 <template>
-    <div v-if="micropost">
-        <v-list-item
-        >
-            <v-list-item-avatar>
-                <v-img src="https://picsum.photos/80/80/?random"></v-img>
-            </v-list-item-avatar>
+     <v-container>
+        <v-row>
+            <v-col>
+                <div v-if="micropost">
+                    <v-list-item
+                    >
+                        <v-list-item-avatar>
+                            <v-img src="https://picsum.photos/80/80/?random"></v-img>
+                        </v-list-item-avatar>
 
-            <v-list-item-content>
-                <v-list-item-title v-html="micropost.user.name"></v-list-item-title>
-                <v-list-item-subtitle style="white-space: pre-line" v-html="micropost.content"></v-list-item-subtitle>
-            </v-list-item-content>
-            <v-list-item-action>
-                <v-list-item-action-text v-text="$dayjs(micropost.created_at).format('YYYY-MM-DD HH:mm:ss')"></v-list-item-action-text>
-                <v-btn icon @click="openEditMicropost">
-                    <v-icon color="green lighten-1">mdi-pen</v-icon>
-                </v-btn>
-                <v-btn icon @click="deleteMicropost">
-                    <v-icon color="error lighten-1">mdi-delete</v-icon>
-                </v-btn>
-            </v-list-item-action>
-        </v-list-item>
-        <v-divider
-                :inset="true"
-        ></v-divider>
-        <micropost-edit-modal ref="dialog" :micropost="micropost" @update="updateMicropost"></micropost-edit-modal>
-    </div>
+           <v-list-item-content>
+                            <v-list-item-title v-html="micropost.user.name"></v-list-item-title>
+                            <v-list-item-subtitle style="white-space: pre-line" v-html="micropost.content"></v-list-item-subtitle>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-list-item-action-text v-text="$dayjs(micropost.created_at).format('YYYY-MM-DD HH:mm:ss')"></v-list-item-action-text>
+                            <div v-if="isMine" class="d-flex">
+                                <v-btn fab small dark color="teal" @click="openEditMicropost" class="mx-2">
+                                    <v-icon>mdi-pen</v-icon>
+                                </v-btn>
+                                <v-btn fab small dark color="error" @click="deleteMicropost" class="mx-2">
+                                    <v-icon>mdi-delete</v-icon>
+                                </v-btn>
+                            </div>
+                        </v-list-item-action>
+                    </v-list-item>
+                    <v-divider
+                            :inset="true"
+                    ></v-divider>
+                    <micropost-edit-modal v-if="isMine" ref="dialog" :micropost="micropost" @update="updateMicropost"></micropost-edit-modal>
+                </div>
+             </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -45,6 +53,11 @@
         computed: {
             micropostId() {
                 return this.$route.params.id
+                 },
+            isMine() {
+                if(!this.$store.getters['auth/currentUser']) return false
+                
+                return this.micropost.user.id == this.$store.getters['auth/currentUser'].id
             }
         },
         methods: {
