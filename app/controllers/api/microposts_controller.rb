3c60aa7eab91478be53e30440a76a0d0e495f773
Y <>
2021-02-28 09:49:18 +0900

    update機能追加

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,5 +1,5 @@
 class Api::MicropostsController < ApplicationController
-  before_action :authenticate, only: %i[create]
+  before_action :authenticate, only: %i[create update]
 
   def index
     microposts = Micropost.includes(:user)
@@ -12,6 +12,9 @@ class Api::MicropostsController < ApplicationController
   end
 
   def update
+    micropost = current_user.microposts.find(params[:id])
+    micropost.update!(micropost_params)
+    render json: micropost, serializer: MicropostSerializer
   end
 
   def destroy
--- spec/requests/api/microposts_request_spec.rb
+++ spec/requests/api/microposts_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-
 RSpec.describe "Api::Microposts", type: :request do
   describe 'GET /api/microposts' do
     let!(:microposts) { create_list(:micropost, 5) }
@@ -16,21 +15,63 @@ RSpec.describe "Api::Microposts", type: :request do
       })
     end
   end
-
   describe 'POST /api/microposts' do
-    let!(:user) { create(:user) }
+    let(:user) { create(:user) }
     let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
     let(:headers) { { Authorization: "Bearer #{token}"} }
     let(:micropost_params) { { micropost: { content: 'hoge' } } }
-    it 'ユーザーが作成できること' do
-      post api_microposts_path, params: micropost_params, headers: headers
-      expect(response).to have_http_status(201)
-      json = JSON.parse(response.body)
-      expect(json['micropost']).to include({
-                                               'id' => be_present,
-                                               'content' => 'hoge',
-                                               'user' => include('id' => user.id)
-                                           })
+    context 'ログイン済みの場合' do
+      it 'マイクロポストが作成できること' do
+        post api_microposts_path, params: micropost_params, headers: headers
+        expect(response).to have_http_status(201)
+        json = JSON.parse(response.body)
+        expect(json['micropost']).to include({
+                                                 'id' => be_present,
+                                                 'content' => 'hoge',
+                                                 'user' => include('id' => user.id)
+                                             })
+      end
+    end
+    context 'ログインしていない場合' do
+      it '401エラーになること' do
+        post api_microposts_path, params: micropost_params
+        expect(response).to have_http_status(401)
+        json = JSON.parse(response.body)
+        expect(json['error']).to include({
+                                             'messages' => be_present
+                                         })
+      end
+    end
+  end
+
+  describe 'PATCH /api/microposts/:id' do
+    let(:user) { create(:user) }
+    let(:token) { Jwt::TokenProvider.call(user_id: user.id) }
+    let(:headers) { { Authorization: "Bearer #{token}"} }
+    let(:micropost) { create(:micropost, user: user) }
+    let(:micropost_params) { { micropost: { content: 'hoge' } } }
+    context 'ログイン済みの場合' do
+      it 'マイクロポストが更新できること' do
+        patch api_micropost_path(micropost), params: micropost_params, headers: headers
+        expect(response).to have_http_status(200)
+        json = JSON.parse(response.body)
+        expect(json['micropost']).to include({
+                                                 'id' => be_present,
+                                                 'content' => 'hoge',
+                                                 'user' => include('id' => user.id)
+                                             })
+      end
+    end
+
+    context 'ログインしていない場合' do
+      it '401エラーになること' do
+        patch api_micropost_path(micropost), params: micropost_params
+        expect(response).to have_http_status(401)
+        json = JSON.parse(response.body)
+        expect(json['error']).to include({
+                                             'messages' => be_present
+                                         })
+      end
     end
   end
 end
