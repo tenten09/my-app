b1a4fb9a8390d36f65465d7f63787aeb936909e4
Y <>
2021-03-04 16:16:43 +0900

    crsf対策

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,9 @@ Vue.use(Vuetify)
 const vuetify = new Vuetify();
 import router from '@/router'
 import store from '@/store'
+const token = document.getElementsByName("csrf-token")[0].getAttribute("content");
+axios.defaults.headers.common["X-CSRF-Token"] = token;
+
 document.addEventListener('DOMContentLoaded', () => {
     const app = new Vue({
         vuetify,
