4fce16e52d7a166677a06996fdbb8b69f9848fce
Y <>
2021-03-05 08:36:13 +0900

    crsfのため修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery prepend: true
   class AuthenticationError < StandardError; end
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
