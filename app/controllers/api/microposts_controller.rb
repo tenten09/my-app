83f9948149b3c875b4f46cd0b9901487cf0dee34
Y <>
2021-03-07 23:27:33 +0900

    inspect user

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -13,7 +13,7 @@ class Api::MicropostsController < ApplicationController
   end
 
   def create
-    logger.debug(current_user.inspect)
+    logger.info(current_user.inspect)
     # logger.info "current_user: #{current_user.attributes.inspect}"
     micropost = current_user.microposts.create!(micropost_params)
     render json: micropost, serializer: MicropostSerializer
