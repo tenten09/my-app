c4b38ae4e665d2546847616d4681e81b292e719f
Y <>
2021-03-09 15:50:46 +0900

    logger コメントアウト

--- app/services/jwt/user_authenticator.rb
+++ app/services/jwt/user_authenticator.rb
@@ -7,7 +7,7 @@ module Jwt::UserAuthenticator
       payload, = Jwt::TokenDecryptor.call(token)
       # logger.info(payload.inspect)
       User.find(payload['user_id'])
-      logger.info(user.inspect)
+      # logger.info(user.inspect)
     rescue => e
       puts e
     end
