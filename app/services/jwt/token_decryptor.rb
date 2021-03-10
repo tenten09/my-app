f18c3d029cf9efda2421dff85f04ab35e345a196
Y <>
2021-03-10 20:54:07 +0900

    puts key

--- app/services/jwt/token_decryptor.rb
+++ app/services/jwt/token_decryptor.rb
@@ -10,7 +10,8 @@ module Jwt::TokenDecryptor
   def decrypt(token)
     JWT.decode(token, Rails.application.credentials.secret_key_base)
   rescue => e 
-    puts "secret_key_base"
+    puts "secret_key_base" + Rails.application.credentials.secret_key_base
+    puts "env_secret" + ENV["SECRET_KEY_BASE"]
     raise e
   end
 end
