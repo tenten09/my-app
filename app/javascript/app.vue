c1951465d7dd0f8ebe7bfa362fbf93995a4d8ca2
github tenten09 <y.koh3103@gmail.com>
2021-03-23 16:26:32 +0900

    投稿を分割

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -49,9 +49,55 @@
       <v-toolbar-title>情報共有</v-toolbar-title>
     </v-app-bar>
 
-    <v-main>
+  <template>
+    <v-card>
+      <v-toolbar
+        color="cyan"
+        dark
+        flat
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <template v-slot:extension>
+          <v-tabs
+            v-model="tab"
+            align-with-title
+          >
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+
+            <v-tab
+              v-for="item in items"
+              :key="item"
+            >
+              {{ item }}
+            </v-tab>
+          </v-tabs>
+        </template>
+      </v-toolbar>
+
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in items"
+          :key="item"
+        >
+          <v-card flat>
+            <!-- <v-card-text v-text="text"></v-card-text> -->
+            <v-main>
+              <router-view></router-view>
+            </v-main>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </template>
+
+    <!-- <v-main>
       <router-view></router-view>
-    </v-main>
+    </v-main> -->
     <v-footer
             color="green"
             app
@@ -66,9 +112,16 @@
     props: {
       source: String,
     },
-    data: () => ({
-      drawer: null,
-    }),
+    data () {
+      return {
+        drawer: null,
+        tab: null,
+        items: [
+          '共通', '倉庫課', '配車',
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
     methods: {
       logout() {
         if (confirm("ログアウトしますか？")) {
