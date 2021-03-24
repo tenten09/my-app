526b3149248f0c7e840b0ee61b9c04e94a19b413
github tenten09 <y.koh3103@gmail.com>
2021-03-24 23:17:52 +0900

    router 設定

--- app/javascript/router/index.js
+++ app/javascript/router/index.js
@@ -5,6 +5,8 @@ import PageUserRegister from '@/pages/PageUserRegister'
 import PageUserLogin from '@/pages/PageUserLogin'
 import PageTimeline from '@/pages/PageTimeline'
 import PageMicropostDetail from '@/pages/PageMicropostDetail'
+import PagePostDetail from '@/pages/PagePostDetail'
+import PagePostTimeline from '@/pages/PagePostTimeline'
 import PageProfile from '@/pages/PageProfile'
 
 const router = new VueRouter({
@@ -14,6 +16,10 @@ const router = new VueRouter({
         { path: '/', component: PageTimeline, name: 'timeline' },
         { path: '/microposts', component: PageTimeline },
         { path: '/microposts/:id', component: PageMicropostDetail, name: 'micropost-detail' },
+
+        { path: '/posts', component: PagePostTimeline },
+        { path: '/posts/:id', component: PagePostDetail, name: 'post-detail' },
+        
         { path: '/profile', component: PageProfile, name: 'my-profile' },
         { path: '/users/:id', component: PageProfile, name: 'user-profile' },
     ]
